7a125ab7422494ab60d2d88e40250815d0e429ef
User 77382840 <user77382840@Computer-77382840.local>
2017-06-16 12:41:29 -0400

    solved the cosmic problem

--- cosmic_generator.py
+++ cosmic_generator.py
@@ -1,7 +1,15 @@
 # Cosmic readout generation
 
-import random
+from random import randint, choice
 
+f = open('cosmic_readouts.txt','w')
 
-grid = '\n'.join([''.join([random.choice('#---') for i in range(10)]) for j in range(10)])
-print(grid)
+for num in range(1,51):
+
+	width,height = randint(20,60),randint(20,60)
+	dist_string = '#' + ('-' * randint(1,5))
+	grid = '\n'.join([''.join([choice(dist_string) for i in range(width)]) for j in range(height)])
+	print(width,height)
+	print(grid)
+	f.write(str(width) + ' ' + str(height) + '\n')
+	f.write(grid + '\n')
