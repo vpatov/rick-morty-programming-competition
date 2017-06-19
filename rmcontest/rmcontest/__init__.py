457c9bbfb4577f84d979a2023a8318e025ab581c
vpatov <vasiapatov@gmail.com>
2017-06-18 22:59:06 -0400

    created the base flask app. created the banner in photoshop as well

---
+++ rmcontest/rmcontest/__init__.py
@@ -0,0 +1,1 @@
+from .rmcontest import app
---
+++ rmcontest/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='rmcontest',
+    packages=['rmcontest'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
