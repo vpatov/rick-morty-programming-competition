3867cca4c1489ecb4d68bb63b39ebab67772f017
Vasia Patov <vasiapatov@gmail.com>
2020-05-23 13:58:57 -0400

    some factor

--- rmcontest/rmcontest/wsgi.py
+++ rmcontest/rmcontest/wsgi.py
@@ -1,3 +1,5 @@
 from rmcontest import app as application
+from rmcontest import start_contest
 if __name__ == '__main__':
 	application.run()
+	start_contest()
--- setup.sh
+++ setup.sh
@@ -1,9 +1,17 @@
-pip install flask_navigation
-pip install gunicorn 
+#pip install flask_navigation
+#pip install gunicorn 
 
-export FLASK_APP='rmcontest'
-export RMCONTEST_ROOT=$(pwd)
-export PYTHONPATH="$RMCONTEST_ROOT/rmcontest/rmcontest:$PYTHONPATH"
+FLASK_APP='rmcontest'
+RMCONTEST_ROOT=$(pwd)
+PYTHONPATH="$RMCONTEST_ROOT/rmcontest/rmcontest:$PYTHONPATH"
+
+export FLASK_APP
+export RMCONTEST_ROOT
+export PYTHONPATH
+
+echo $FLASK_APP
+echo $RMCONTEST_ROOT
+echo $PYTHONPATH
 
 cd rmcontest/rmcontest
 flask initdb
